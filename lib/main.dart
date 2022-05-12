6ac7d7beb53f956f10fec1d462ce3a434167095c
Felipe Dias <adfelipealmeida@gmail.com>
2022-05-12 08:11:03 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import '../pages/todo_list_page.dart';
+
+void main() {
+  runApp(const MyApp()); // Hot Reload
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: TodoListPage(), // Tela Inicial do aplicativo
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
