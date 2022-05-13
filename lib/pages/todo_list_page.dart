cf1346a1a2be620fe3cfb7c098e52ac93d986661
Felipe Dias <adfelipealmeida@gmail.com>
2022-05-13 08:36:44 -0300

    criado campo de texto e botão

--- lib/pages/todo_list_page.dart
+++ lib/pages/todo_list_page.dart
@@ -5,8 +5,34 @@ class TodoListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.green,
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              // Ocupa o maior espaço disponível da tela
+              const Expanded(
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Adicione uma tarefa",
+                    hintText: "Ex: Estudar Flutter",
+                  ),
+                ),
+              ),
+              const SizedBox(
+                width: 8.0,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text("+"),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
